26a5f584574bfe1781a9ac28679b0d63d76d0cc2
Keith S Thomas <kthomasnj@hotmail.com>
2022-08-21 17:23:00 -0400

    Added functionality to create README.md file.

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 // TODO: Create an array of questions for user input
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./assets/js/generateMarkdown')
+const generateMarkdown = require('./assets/js/generateMarkdown');
 
-const questions = [
+const questions = () => {
+    return inquirer.prompt ([
     {
         type: 'input',
         name: 'title',
@@ -54,30 +55,25 @@ const questions = [
         name: "email",
         message: "What is your email?"
     },
-  ];
+    ])};
 
 // TODO: Create a function to write README file
 
+function writeToFile(fileName, data) {
 
-// fs.writeFile('README.md', `${ans}\n`, (err) => {
-//     err ? console.error(err) : console.log('Commit logged!')
-// });
-
-// function writeToFile(fileName, data) {}
+}
 
 // // TODO: Create a function to initialize app
-// function init() {}
 
 const init = () => {
-    questions()
-      // Use writeFileSync method to use promises instead of a callback function
+    questions()  
       .then((data) => fs.writeFileSync("README.md", generateMarkdown(data)))
-      // If it was successful console log Successfully wrote to README.md
+      
       .then(() => console.log("Successfully wrote to README.md"))
       .catch((err) => console.error("error", err));
+
   };
+
   // Function call to initialize app
   init();
 
-// // Function call to initialize app
-// init();
